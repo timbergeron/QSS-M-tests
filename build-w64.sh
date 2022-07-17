c638952949a046d57dff638fdc6476a584c68553
Ad Infinitum <67801374+inf1niti@users.noreply.github.com>
2022-07-17 14:47:40 -0400

    individual build scripts and concurrent compilation
    added individual build scripts per-arch (w32, w64, *nix)
    add concurrency (8) to makeargs

--- build-w64.sh
+++ build-w64.sh
@@ -9,15 +9,6 @@ export SOURCE_DATE_EPOCH=$(git log -1 --date=short --format=%ct)
 cd Quake/
 MAKEARGS="-j8"
 
-# Make win32
-export QSS_CFLAGS="-DQSS_REVISION=`git rev-parse HEAD`"
-export QSS_LDFLAGS=""
-make -f Makefile.w32 clean
-./build_cross_win32-sdl2.sh $MAKEARGS
-mv quakespasm.exe QSS-M-w32.exe
-zip -9j QSS-M-w32.zip ../Windows/codecs/x86/*.dll ../LICENSE.txt ../Quakespasm.html quakespasm.pak ../Quakespasm.txt ../Quakespasm-Spiked.txt ../Quakespasm-Music.txt ../Windows/SDL2/lib/SDL2.dll ../QSS-M-Revision.txt QSS-M-w32.exe
-make -f Makefile.w32 clean
-
 # Make win64
 export QSS_CFLAGS="-DQSS_REVISION=`git rev-parse HEAD`"
 export QSS_LDFLAGS=""
@@ -26,12 +17,3 @@ make -f Makefile.w64 clean
 mv quakespasm.exe QSS-M-w64.exe
 zip -9j QSS-M-w64.zip ../Windows/codecs/x64/*.dll ../LICENSE.txt ../Quakespasm.html quakespasm.pak ../Quakespasm.txt ../Quakespasm-Spiked.txt ../Quakespasm-Music.txt ../Windows/SDL2/lib64/SDL2.dll ../QSS-M-Revision.txt QSS-M-w64.exe
 make -f Makefile.w64 clean
-
-# Make Linux64
-export QSS_CFLAGS="-DQSS_REVISION=`git rev-parse HEAD`"
-export QSS_LDFLAGS=""
-make clean
-make USE_SDL2=1 $MAKEARGS
-mv quakespasm QSS-M-l64
-zip -9j QSS-M-l64.zip ../LICENSE.txt ../Quakespasm.html quakespasm.pak ../Quakespasm.txt ../Quakespasm-Spiked.txt ../Quakespasm-Music.txt ../QSS-M-Revision.txt QSS-M-l64
-make clean
