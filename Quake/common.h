d318433b84715e47543e2a282dc912d0d0c2376a
timbergeron <timbergeron@gmail.com>
2023-01-13 22:01:24 -0800

    more warnings

--- Quake/common.h
+++ Quake/common.h
@@ -241,7 +241,7 @@ extern int q_vsnprintf(char *str, size_t size, const char *format, va_list args)
 
 extern char* Q_strrev (char* s); // woods
 extern char* strremove(char* str, char* sub); // woods
-void Write_Log (char* log_message, char* filename); // woods #serverlist
+void Write_Log (const char* log_message, char* filename); // woods #serverlist
 
 #define strcasecmp brokeninmsvc
 #define stricmp brokenportability
