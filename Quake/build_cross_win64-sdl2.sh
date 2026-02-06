7b35ae8df2a76c4c99217702d3ae8f4ddafb7327
timbergeron <timbergeron@gmail.com>
2026-02-06 14:48:30 -0800

    uodate

--- Quake/build_cross_win64-sdl2.sh
+++ Quake/build_cross_win64-sdl2.sh
@@ -2,36 +2,46 @@
 
 # Change this script to meet your needs and/or environment.
 
-TARGET=x86_64-w64-mingw32
-PREFIX=/opt/cross_win64
+TARGET="${TARGET:-x86_64-w64-mingw32}"
+PREFIX="${CROSS_PREFIX:-${PREFIX:-/opt/cross_win64}}"
 
-PATH="$PREFIX/bin:$PATH"
+if [ -d "$PREFIX/bin" ]; then
+	PATH="$PREFIX/bin:$PATH"
+fi
 export PATH
 
-MAKE_CMD=make
+MAKE_CMD="${MAKE_CMD:-make}"
 
-CC="$TARGET-gcc"
-AS="$TARGET-as"
-RANLIB="$TARGET-ranlib"
-AR="$TARGET-ar"
-WINDRES="$TARGET-windres"
-STRIP="$TARGET-strip"
-if command -v "$TARGET-pkg-config" >/dev/null 2>&1; then
-	PKG_CONFIG="$TARGET-pkg-config"
-elif command -v "$TARGET-pkgconf" >/dev/null 2>&1; then
-	PKG_CONFIG="$TARGET-pkgconf"
-else
-	PKG_CONFIG="pkg-config"
+CC="${CC:-$TARGET-gcc}"
+AS="${AS:-$TARGET-as}"
+RANLIB="${RANLIB:-$TARGET-ranlib}"
+AR="${AR:-$TARGET-ar}"
+WINDRES="${WINDRES:-$TARGET-windres}"
+STRIP="${STRIP:-$TARGET-strip}"
+
+if [ -z "${PKG_CONFIG:-}" ]; then
+	if command -v "$TARGET-pkg-config" >/dev/null 2>&1; then
+		PKG_CONFIG="$TARGET-pkg-config"
+	elif command -v "$TARGET-pkgconf" >/dev/null 2>&1; then
+		PKG_CONFIG="$TARGET-pkgconf"
+	else
+		PKG_CONFIG="pkg-config"
+	fi
 fi
-if [ "$PKG_CONFIG" = "pkg-config" ]; then
+
+if [ "$PKG_CONFIG" = "pkg-config" ] && [ -z "${PKG_CONFIG_LIBDIR:-}" ]; then
 	if [ -d "$PREFIX/$TARGET/lib/pkgconfig" ] || [ -d "$PREFIX/$TARGET/share/pkgconfig" ]; then
 		PKG_CONFIG_LIBDIR="$PREFIX/$TARGET/lib/pkgconfig:$PREFIX/$TARGET/share/pkgconfig"
+	elif [ -d "$PREFIX/lib/pkgconfig" ] || [ -d "$PREFIX/share/pkgconfig" ]; then
+		PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
 	elif [ -d "/usr/$TARGET/lib/pkgconfig" ] || [ -d "/usr/$TARGET/share/pkgconfig" ]; then
 		PKG_CONFIG_LIBDIR="/usr/$TARGET/lib/pkgconfig:/usr/$TARGET/share/pkgconfig"
 	elif [ -d "/usr/lib/$TARGET/pkgconfig" ]; then
 		PKG_CONFIG_LIBDIR="/usr/lib/$TARGET/pkgconfig"
+	elif [ -d "/usr/local/$TARGET/lib/pkgconfig" ] || [ -d "/usr/local/$TARGET/share/pkgconfig" ]; then
+		PKG_CONFIG_LIBDIR="/usr/local/$TARGET/lib/pkgconfig:/usr/local/$TARGET/share/pkgconfig"
 	fi
 fi
-export PATH CC AS AR RANLIB WINDRES STRIP PKG_CONFIG PKG_CONFIG_LIBDIR
+export PATH CC AS AR RANLIB WINDRES STRIP PKG_CONFIG PKG_CONFIG_LIBDIR PKG_CONFIG_PATH
 
-exec $MAKE_CMD USE_SDL2=1 CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w64 $*
+exec "$MAKE_CMD" USE_SDL2=1 CC="$CC" AS="$AS" RANLIB="$RANLIB" AR="$AR" WINDRES="$WINDRES" STRIP="$STRIP" PKG_CONFIG="$PKG_CONFIG" -f Makefile.w64 "$@"
--- build-w64.sh
+++ build-w64.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 
 echo "Git URL:      https://github.com/timbergeron/QSS-M.git" > QSS-M-Revision.txt
 echo "Git Revision: `git rev-parse HEAD`" >> QSS-M-Revision.txt
@@ -7,24 +8,39 @@ echo "Compile Date: `date`" >> QSS-M-Revision.txt
 export SOURCE_DATE_EPOCH=$(git log -1 --date=short --format=%ct)
 
 cd Quake/
-MAKEARGS="-j8"
+MAKEARGS="${MAKEARGS:--j8}"
+TARGET_TRIPLET="${TARGET_TRIPLET:-x86_64-w64-mingw32}"
 
 # Make win64
 export QSS_CFLAGS="-DQSS_REVISION=`git rev-parse HEAD`"
 export QSS_LDFLAGS="-Wl,--allow-multiple-definition"
+
 make -f Makefile.w64 clean
+
+# shellcheck disable=SC2086
 ./build_cross_win64-sdl2.sh $MAKEARGS
+
+if [ ! -f quakespasm.exe ]; then
+	echo "ERROR: quakespasm.exe was not produced."
+	exit 1
+fi
 mv quakespasm.exe QSS-M-w64.exe
 
 GNUTLS_DLLS=""
+DLL_SEARCH_DIRS="/usr/$TARGET_TRIPLET/bin /usr/lib/gcc/$TARGET_TRIPLET/* /opt/cross/$TARGET_TRIPLET/bin /opt/cross_win64/bin /opt/cross_win64/$TARGET_TRIPLET/bin ${CROSS_GNUTLS_DLL_DIRS:-}"
 for pat in libgnutls-*.dll libnettle-*.dll libhogweed-*.dll libgmp-*.dll libidn2-*.dll libunistring-*.dll libtasn1-*.dll libiconv-*.dll libp11-kit-*.dll libffi-*.dll
 do
-	for dll in /usr/x86_64-w64-mingw32/bin/$pat /usr/lib/gcc/x86_64-w64-mingw32/*/$pat
-	do
-		[ -f "$dll" ] || continue
-		GNUTLS_DLLS="$GNUTLS_DLLS $dll"
+	for dir in $DLL_SEARCH_DIRS; do
+		for dll in $dir/$pat; do
+			[ -f "$dll" ] || continue
+			case " $GNUTLS_DLLS " in
+				*" $dll "*) ;;
+				*) GNUTLS_DLLS="$GNUTLS_DLLS $dll" ;;
+			esac
+		done
 	done
 done
 
+# shellcheck disable=SC2086
 zip -9j QSS-M-w64.zip ../Windows/codecs/x64/*.dll ../Windows/curl/lib/x64/libcurl.dll ../Windows/zlib/x64/zlib1.dll ../LICENSE.txt ../Quakespasm.html quakespasm.pak qssm.pak ../Quakespasm.txt ../Quakespasm-Spiked.txt ../Quakespasm-Music.txt ../Windows/SDL2/lib64/SDL2.dll ../QSS-M-Revision.txt QSS-M-w64.exe $GNUTLS_DLLS
 make -f Makefile.w64 clean
